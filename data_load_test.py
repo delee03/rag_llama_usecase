ec932d44ab8e4a1af6776f71d0f734542bcc37de
fuderrpham <128588793+delee03@users.noreply.github.com>
2025-03-03 16:27:51 +0700

    refactor code

--- data_load_test.py
+++ data_load_test.py
@@ -1,37 +1,23 @@
 #1. Import OS, Document Loader, Text Splitter, Bedrock Embeddings, Vector DB, VectorStoreIndex, Bedrock-LLM
 import os
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter 
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_aws import BedrockEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.indexes import VectorstoreIndexCreator
 from langchain_aws import BedrockLLM
-
+ 
 #5c. Wrap within a function
 def hr_index():
-
     #2. Define the data source and load data with PDFLoader(https://www.upl-ltd.com/images/people/downloads/Leave-Policy-India.pdf)
-    data_load=PyPDFLoader('https://www.upl-ltd.com/images/people/downloads/Leave-Policy-India.pdf')
-    data_test=data_load.load_and_split()
-    print(len(data_test))
-    print(type(data_test))
-    # print(data_test[2])
-
-
+    data_load=PyPDFLoader('https://www.upl-ltd.com/images/people/downloads/Leave-Policy-India.pdf')  
+ 
     #3. Split the Text based on Character, Tokens etc. - Recursively split by character - ["\n\n", "\n", " ", ""]
-
-    data_split = RecursiveCharacterTextSplitter(separators=["\n\n", "\n", " ", ""], chunk_size=4, chunk_overlap=1)
-    data_sample = "Write a function which searches the user prompt, searches the best match from Vector DB and sends both to LLM."
-    data_split_test = data_split.split_text(data_sample)
-    print((data_split_test))
-
+    data_split=RecursiveCharacterTextSplitter(separators=["\n\n", "\n", " ", ""], chunk_size=100,chunk_overlap=10)
     #4. Create Embeddings -- Client connection
-    data_embeddings = BedrockEmbeddings(
-        credentials_profile_name='default',
-        model_name='amazon.titan-embed-text-v2:0',
-    )
-
-    #5à Create Vector DB, Store Embeddings and Index for Search - VectorstoreIndexCreator
+    data_embeddings=BedrockEmbeddings(
+    credentials_profile_name= 'default',
+    model_id='amazon.titan-embed-text-v1')
     #5à Create Vector DB, Store Embeddings and Index for Search - VectorstoreIndexCreator
     data_index=VectorstoreIndexCreator(
         text_splitter=data_split,
@@ -40,13 +26,11 @@ def hr_index():
     #5b  Create index for HR Policy Document
     db_index=data_index.from_loaders([data_load])
     return db_index
-
-
-#6a. Write a function to connect to Bedrock Foundation Model
+#6a. Write a function to connect to Bedrock Foundation Model - Claude Foundation Model
 def hr_llm():
     llm=BedrockLLM(
         credentials_profile_name='default',
-        model_id='anthropic.claude-3-5-sonnet-20240620-v1:0',
+        model_id='anthropic.claude-v2',
         model_kwargs={
         "max_tokens_to_sample":3000,
         "temperature": 0.1,
